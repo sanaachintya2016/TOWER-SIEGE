12ce39c2df0db4f88a8c26ba80c57519ba7193a3
sanaachintya2016 <82821838+sanaachintya2016@users.noreply.github.com>
2021-08-11 10:04:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+
+function preload() {
+
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+ 
+}
+
+function draw(){
+
+    background(255);   
+     text(mouseX + ',' + mouseY, 30, 45);
+
+
+    Engine.update(engine);
+
+}
